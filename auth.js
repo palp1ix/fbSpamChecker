73d8a2969e25ede4a892d9cff47c0bcbad8d1b01
palp1ix <arthach.2008@gmail.com>
2024-08-02 18:33:30 +0300

    Завоз аунтифекации, работает только на сайте, в расширении нет

---
+++ auth.js
@@ -0,0 +1,54 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-app.js";
+import { 
+  getAuth, 
+  createUserWithEmailAndPassword, 
+  signInWithEmailAndPassword,
+  connectAuthEmulator,
+  onAuthStateChanged
+ } from 'https://www.gstatic.com/firebasejs/10.12.4/firebase-auth.js';
+import { getDatabase } from "https://www.gstatic.com/firebasejs/10.12.4/firebase-database.js";
+
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyCT3xLLQz_o4aeTZE-9DAX910J3Fd0U75k",
+  authDomain: "fbspam-f7e95.firebaseapp.com",
+  projectId: "fbspam-f7e95",
+  storageBucket: "fbspam-f7e95.appspot.com",
+  messagingSenderId: "970360759604",
+  appId: "1:970360759604:web:c47b0d3e9fdb6019d23161",
+  measurementId: "G-RWWC3EXJTZ",
+  databaseURL: "https://users.europe-central2.firebaseio.com"
+};
+
+const authDiv = document.querySelector('.auth-container');
+const errorDiv = document.querySelector('#error-container');
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const auth = getAuth(app);
+//connectAuthEmulator(auth, "http://localhost:9099");
+const registerEmailPassword = async () => {
+  errorDiv.innerHTML = "";
+  const email = document.getElementById('auth-email').value;
+  const password = document.getElementById('auth-password').value;
+
+  const userCredential = await signInWithEmailAndPassword(auth, email, password)
+  .then((userCredential) => {
+    const user = userCredential.user;
+    console.log(user);
+    authDiv.style.display = 'none';
+    // ...
+  })
+  .catch((error) => {
+    const errorCode = error.code;
+    const errorMessage = error.message;
+    
+    errorDiv.innerHTML += `<p style="color: red;">Error: ${error.message}</p>`;
+    // ..
+  });
+}
+document.getElementById('register-btn').addEventListener('click', registerEmailPassword);
